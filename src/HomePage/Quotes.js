efbebed87b00f47fbc0e81261d3a53c4932367a2
Zee530 <zeee530@gmail.com>
2021-09-21 14:28:00 +0100

    vvv

---
+++ src/HomePage/Quotes.js
@@ -0,0 +1,8 @@
+var Quotes = ["'You don’t choose your family. They are God’s gift to you, as you are to them.'' –Desmond Tutu",
+                "'A happy family is but an earlier heaven.' –George Bernard Shaw",
+                "“The family is the test of freedom; because the family is the only thing that the free man makes for himself and by himself.” –Gilbert K. Chesterton",
+                "“There is no doubt that it is around the family and the home that all the greatest virtues… are created, strengthened and maintained.” –Winston Churchill",
+                "“Rejoice with your family in the beautiful land of life.” –Albert Einstein",
+                "“The only rock I know that stays steady, the only institution I know that works, is the family.” –Lee Iacocca"
+            ]
+export default Quotes
